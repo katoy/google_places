ceef74972125baca17e2b0198e2f4e81ed680d9f
katoy <youichikato@gmail.com>
2021-10-14 10:39:26 +0900

    プログラム追加

---
+++ test_update.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require './google_place_id'
+require 'pp'
+require 'pry'
+require 'pry-byebug'
+
+client = GooglePlaceId.new
+
+puts '#--- update_csv'
+client.update_csv
+rows = client.read_csv
+pp rows.map(&:values)
